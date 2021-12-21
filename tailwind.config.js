eabd7f88a6ab0849eed2415011723b52348721ce
Mostafa <mosi3883@gmail.com>
2021-12-21 10:49:13 +0330

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
